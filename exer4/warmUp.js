ac349a4638c204ae499ea39072c90524d51cb072
Arya Menon <menonarya14@gmail.com>
2026-09-10 21:35:43 -0400

    I finished adding all the functions to warmUp.js

--- exer4/warmUp.js
+++ exer4/warmUp.js
@@ -16,7 +16,20 @@
  * F = anything < 60
  */
 function toLetterGrade(numGrade) {
-  // TODO
+  if(numGrade>100)
+    return "INVALID";
+  if(numGrade>=90)
+    return "A";
+  if(numGrade>=80)
+    return "B";
+  if(numGrade>=70)
+    return "C";
+  if(numGrade>=60)
+    return "D";
+  if(numGrade>=0)
+    return "F";
+  else
+    return "INVALID";
 }
 
 /**
@@ -27,7 +40,7 @@ function toLetterGrade(numGrade) {
  * Example: [9, 3, 4, 1, 2, 0] --> [4, 2, 0]
  */
 function getEvenElements(array) {
-  // TODO
+  return array.filter(element => element%2==0);
 }
 
 /**
@@ -40,7 +53,12 @@ function getEvenElements(array) {
  * Example: "I love Bits of Good" --> 4
  */
 function findLongestWord(string) {
-  // TODO
+  let words = string.split(" ");
+  return words.reduce((accumulator, word) => {
+    if(word.length>accumulator)
+      accumulator = word.length;
+    return accumulator;
+  }, 0);
 }
 
 /**
@@ -65,7 +83,7 @@ function findLongestWord(string) {
     } 
  */
 function combineObjects(object1, object2) {
-  // TODO
+   return {...object1, ...object2};
 }
 
 /**
@@ -77,7 +95,12 @@ function combineObjects(object1, object2) {
  *  Example: [1, 2, 3] -> [3, 2, 1]
  */
 function reverseArr(array) {
-  // TODO
+  let toReturn = [];
+  let s = array.length;
+  for(let i = 0; i<s; i++){
+    toReturn.push(array.pop());
+  }
+  return toReturn;
 }
 
 // DO NOT EDIT BELOW THIS LINE -- the code is for testing purposes only!
